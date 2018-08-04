c90adecb25e2c9170085116ae2e31e89bc5f3b82
Jishnu <jichu001@gmail.com>
2018-08-04 13:03:04 +0200

    start2

--- src/readIOU.cpp
+++ src/readIOU.cpp
@@ -12,7 +12,7 @@ std::map<std::string, std::string> readOUIFile(const std::string& path)
     
     std::ifstream ouiFile(path);
 
-    if(1){//ouiFile.is_open()) {
+    if(ouiFile.is_open()) {
         while(getline(ouiFile,line))  {
             if(line.find("(hex)") != std::string::npos) {
                 std::string ss(line);
@@ -34,11 +34,19 @@ std::map<std::string, std::string> readOUIFile(const std::string& path)
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
-    std::map<std::string, std::string> ouiMap = readOUIFile("../toui.txt");
+    if(argc < 2) {
+        std::string usage = "Usage: " + std::string(argv[0]) + " <macaddress>";
+        printf("\n%s\n", usage.c_str());
+        return 1;
+    }
+
+    std::map<std::string, std::string> ouiMap = readOUIFile("../oui.txt");
+
     if(ouiMap.empty()) {
-        printf("\nEmpty file");
+        printf("\nError reading oui file, check path");
+        return 1;
     }
     
     /*for(std::map<std::string, std::string>::iterator iterator = ouiMap.begin(); iterator != ouiMap.end(); iterator++) {
@@ -46,7 +54,7 @@ int main()
     // Repeat if you also want to iterate through the second map.
     }*/
 
-    std::string mac = "10:9a:dd:66:ab:c7";
+    std::string mac = argv[1];
     std::transform(mac.begin(), mac.end(),mac.begin(), ::toupper);
      // replace all 'x' to 'y'
     std::replace(mac.begin(), mac.end(), '-', ':');
